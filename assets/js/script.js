45947185d9e77a86ae4be558073ca3a7a83760ae
Mervin Wolff <116472292+Mwolff6@users.noreply.github.com>
2023-09-11 02:14:57 +0800

    update script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+<!-- First, include your existing JavaScript for theme switching -->
+<script>
+  const darkModeToggle = document.getElementById('dark-mode-toggle');
+  const themeStyle = document.getElementById('theme-style');
+
+  darkModeToggle.addEventListener('click', () => {
+    if (themeStyle.getAttribute('href') === 'light-theme.css') {
+      themeStyle.setAttribute('href', 'dark-theme.css');
+    } else {
+      themeStyle.setAttribute('href', 'light-theme.css');
+    }
+  });
+</script>
+
+<!-- Then, add the JavaScript for toggling the dark-mode class -->
 <script>
   function toggleDarkMode() {
     const body = document.body;
